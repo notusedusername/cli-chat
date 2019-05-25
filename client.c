17c3629e0ea736bbfade9a3c84149ba4d892a917
notusedusername <36472592+notusedusername@users.noreply.github.com>
2019-05-25 15:22:46 +0200

    Add files via upload

---
+++ client.c
@@ -0,0 +1,189 @@
+#include <stdio.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <string.h>
+#include <netinet/in.h>
+#include <stdlib.h>
+#include <arpa/inet.h>
+#include <pthread.h>
+#include <unistd.h>
+#include <ncurses.h>
+
+
+
+#define BUFFER_SIZE 1024
+
+#define PORT 3300
+
+#define BLU   "\x1B[34m"
+#define WHT   "\x1B[37m"
+#define RESET "\x1B[0m"
+
+struct args_struct
+{
+	char buffer[BUFFER_SIZE];
+
+	int len;
+
+	int recvs;
+
+	int sock;
+
+	int flag;
+}args;
+
+
+//void *incoming(char buffer[BUFFER_SIZE], int len, int recvs, int sock){		thread nélkül
+
+
+void *incoming(void *arguments){
+	//BEJÖVŐ ÜZENET
+
+	struct args_struct *args = arguments;
+	for(;;){
+		args -> recvs = 0;
+
+		args -> len = BUFFER_SIZE+1;
+		
+		args -> recvs = recv(args -> sock, args -> buffer, args -> len, 0);
+
+		if ( args -> recvs == 0 ){
+			printf(WHT "Server closed connection. Bye.\n" RESET);
+			
+			close(args -> sock);
+
+			exit(4);	
+		}
+		else if(args -> recvs > 0)  {
+		
+			if(strcmp(args -> buffer,"<<STOP_*_ACK>>") && strcmp(args -> buffer,"<<CLT_*_ACK>>")){
+				
+				if(!strncmp(args -> buffer, "<<", 2))
+					printf( WHT "%s\n\n" RESET ,args -> buffer );
+
+				else printf( "%s\n\n" ,args -> buffer );
+			}
+			
+		}
+		else printf("Recv error!\n");
+
+		if( !strcmp(args -> buffer, "<<STOP_*_ACK>>")){
+
+			args -> flag = 1;
+		}
+
+		if ( args -> flag == 0 ){
+
+			sprintf(args -> buffer, "CLT_*_ACK");
+
+			send(args -> sock, args -> buffer, strlen(args -> buffer)+1, 0 );
+	
+		}
+	
+		if ( !strncmp(args -> buffer, "\r<<", strlen("\r<<"))){
+			sprintf(args -> buffer, "CLT_*_CHNG");
+			send(args -> sock, args -> buffer, strlen(args -> buffer)+1, 0);
+			
+		}
+	}	
+	//incoming(buffer, len, recvs, sock);
+}
+
+void outgoing (char buffer[BUFFER_SIZE], int len, int sends, int sock){
+
+	sends = 0;
+
+	fgets(buffer, BUFFER_SIZE, stdin);										// a space-ket ignorálja, kicsit pofásabb
+		
+	len = strlen(buffer)+1;
+
+	if (!strcmp(buffer, "--change\n")){
+		args.flag = 0;
+	}
+
+	//ÜZENET KÜLDÉSE
+
+	sends = send(sock, buffer, len, 0); 								//célsocket, küldendő infó, hossza, 0
+
+	if ( sends < 0 ){
+		printf("Can't send the message! Try it again!\n");
+	}
+	printf("\n");
+}
+
+int main( int argc, char* argv[]){
+
+	//SOCKET LÉTREHOZÁS
+
+	int sock = 0;
+
+	sock = socket(AF_INET, SOCK_STREAM, 0);
+
+	if( sock < 0 )
+		exit(1);
+
+	int ext = 0; 														//függvény által visszaadott érték hibakezelésre
+
+	struct sockaddr_in server;
+
+	server.sin_family = AF_INET;
+
+	server.sin_port = htons(PORT);
+
+	char server_addr[16];
+
+	sprintf(server_addr, "%s", argv[1]);
+
+	server.sin_addr.s_addr = inet_addr(server_addr);
+
+	int server_addrlen = sizeof server;
+
+	char option = 1;
+
+
+	ext = connect ( sock, (struct sockaddr *) &server, server_addrlen );
+
+	if ( ext < 0 )
+	{
+		printf("Can't connect to the server!\n");
+		exit(2);
+	}
+
+	char in_buffer[BUFFER_SIZE];
+
+	char out_buffer[BUFFER_SIZE];
+
+
+	int len;
+
+	int sends;
+
+	pthread_t thread;
+
+	//args.buffer = in_buffer;
+
+	args.len = 0;
+
+	args.recvs = 0;
+
+	args.sock = sock;
+
+	args.flag = 0;
+
+	if (pthread_create( &thread, NULL, incoming, (void *) &args ) != 0 ){
+
+		printf("Can't create incoming thread. \n");
+	}
+
+	for(;;){
+ 		
+ 		
+		//incoming(in_buffer, len, recvs, sock);			tread nélkül
+		
+		outgoing(out_buffer, len, sends, sock);
+		
+	}
+
+
+
+}
---
+++ server.c
@@ -0,0 +1,691 @@
+#include <stdio.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <string.h>
+#include <netinet/in.h>
+#include <stdlib.h>
+#include <arpa/inet.h>
+#include <pthread.h>
+#include <unistd.h>
+
+#define PORT_NO 3300
+#define BUFF_SIZE 1024
+#define MAX_CLIENT 4
+#define ROOM1 ((const unsigned char*)"ROOM1")
+#define ROOM2 ((const unsigned char*)"ROOM2")
+
+
+struct args_struct
+{
+
+	int sock[MAX_CLIENT];
+
+	int mysock;
+
+	int counter;
+}args;
+
+struct chat_rooms{
+	
+	int r1sock[2];
+
+	int r2sock[2];
+
+	int choose[MAX_CLIENT];
+
+	int change_count[MAX_CLIENT];
+}rooms;
+
+
+int ack( int sock){
+
+	char buffer[BUFF_SIZE];
+
+	recv(sock, buffer, BUFF_SIZE, 0);
+
+	if(!strcmp(buffer, "CLT_*_ACK"))
+		return 0;
+	else 
+		return -1;
+}
+
+void sendctrl(int sock, char message[]){
+
+	int ctrl = ack(sock);
+
+	if (ctrl == -1){
+
+		printf("Send error, retry\n");
+
+		send(sock, message, strlen(message)+1, 0);
+
+		sendctrl(sock, message);
+	}
+
+}
+	
+void terminate(int thread_no, int sock, int mysock){
+	
+
+	args.counter--;
+	//free(sock_desc);
+	
+	if(args.counter == 0 ){
+		
+		printf("All clients disconnected, shutdown...\n");
+
+		close(sock);
+
+		close(mysock);
+
+		exit(0);
+
+	}
+	else{
+		close(sock);
+
+		pthread_exit(0);
+	}
+
+}
+
+int members ( int room[] ){
+
+	int counter = 0;
+
+	for (int i = 0; i < 2; i++)	{
+		if(room[i] != 0){
+			counter++;
+		}
+	}//for
+
+	return counter;
+}
+
+int chat_rooms(int sock, char message[], char in_message[BUFF_SIZE], int thread_no, void *arguments){
+
+	rooms.change_count[thread_no]++;
+
+	sprintf(message, "<<We have 2 chat rooms. These are:\n\t 1.) %s[%d/2]\n\t 2.) %s[%d/2]\n You can choose by typing its number.>>\n", ROOM1, members(rooms.r1sock), ROOM2, members(rooms.r2sock) );
+	//printf("%s\n", message);
+	int sends = 0, try = 0;
+	
+	sends = send (sock, message, strlen(message), 0);
+
+	//if(rooms.change_count[thread_no] < 2)
+
+		sendctrl(sock, message);
+
+
+	LOOP1: if(sends > 0){
+
+		printf("Roomlist sent to client%d\n", thread_no+1);
+	}
+
+	else{
+		try++;
+
+		if(try == 1)
+			printf("Can't send to %d roomlist! Retry..\n", thread_no);
+
+		goto LOOP1;
+	}	
+		
+	int recvs;
+
+	LOOP2: recvs = recv( sock, in_message, BUFF_SIZE, 0 );
+
+	if ( recvs == 0 ){
+		printf("Client%d disconnected.\n", thread_no+1);
+		return 0;
+	}
+
+	else if ( recvs < 0)
+		printf("Recv error!\n");
+
+	else {
+		int choose = 0;
+		
+		choose = atoi(in_message);
+
+		rooms.choose[thread_no] = choose;
+
+		int flag = 0;
+
+		if (choose == 1 || choose == 2){
+
+			switch (choose){
+
+				case 1:{
+					
+					printf("Client%d: Selected room is %s\n",thread_no+1,ROOM1 );
+
+					sprintf(message,"<<Selected room is %s>>\n",ROOM1 );
+					
+					send (sock, message, strlen(message)+1, 0);
+
+					//if(rooms.change_count[thread_no] < 2)
+						sendctrl(sock, message);
+
+					for ( int i = 0; i < 2; i++){
+						
+						if (rooms.r1sock[i] != 0 ){
+							flag++;
+						}
+
+					}
+
+					if ( flag == 0 ){
+						sprintf(message, "<<Waiting for partner...>>\n");
+						
+						send(sock, message, strlen(message)+1, 0);
+						
+						//if(rooms.change_count[thread_no] < 2)
+							sendctrl(sock, message);
+
+
+						printf("Waiting for partner...\n");
+					}
+					else if (flag == 1) {
+						
+
+						printf("Connected, you can start chatting.\n");
+					}
+
+					else {
+							sprintf(message,"<<Room is full. Try an another one.>>\n");
+						
+							send (sock, message, strlen(message)+1, 0);
+
+							//if(rooms.change_count[thread_no] < 2)
+								sendctrl(sock, message);
+
+							goto LOOP2;
+					}
+					for ( int i = 0; i < 2; i++){
+						if(rooms.r1sock[i] == 0){
+
+							rooms.r1sock[i] = sock;
+
+							break;
+						}
+
+					}
+					break;
+				}
+
+				case 2:{
+					printf("Client%d: Selected room is %s\n",thread_no+1,ROOM2 );
+
+					sprintf(message, "<<Selected room is %s>>\n",ROOM2 );
+					
+					send (sock, message, strlen(message)+1, 0);
+
+					//if(rooms.change_count[thread_no] < 2)
+						sendctrl(sock, message);
+
+
+					for ( int i = 0; i < 2; i++){
+						
+						if (rooms.r2sock[i] != 0 ){
+							flag++;
+						}
+
+					}
+
+						if ( flag == 0 ){
+							sprintf(message, "<<Waiting for partner...>>\n");
+							
+							send(sock, message, strlen(message)+1, 0);
+							
+							//if(rooms.change_count[thread_no] < 2)
+								sendctrl(sock, message);
+
+
+							printf("Waiting for partner...\n");
+						}
+						else if (flag == 1) {
+							
+
+							printf("Connected, you can start chatting.\n");
+						}
+
+						else {
+								sprintf(message,"<<Room is full. Try an another one.>>\n");
+							
+								send (sock, message, strlen(message)+1, 0);
+
+								//if(rooms.change_count[thread_no] < 2)
+									sendctrl(sock, message);
+
+
+								goto LOOP2;
+						}
+						for ( int i = 0; i < 2; i++){
+							if(rooms.r2sock[i] == 0){
+
+								rooms.r2sock[i] = sock;
+
+								break;
+							}
+
+						}
+					break;
+				}
+			}
+		}
+		
+		else{
+			printf("\n");
+			sprintf(message, "<<Funny... try again.>>\n");	
+		
+			send (sock, message, strlen(message)+1, 0);
+
+			//if(rooms.change_count[thread_no] < 2)
+						sendctrl(sock, message);
+
+
+			goto LOOP2;
+
+		} 
+	}
+
+	return recvs;
+
+}
+
+void *handler( void *arguments){
+
+	struct args_struct *args = arguments;
+
+	int thread_no = (args -> counter)-1;
+
+	printf("counter: %d  thread_no: %d\n", args->counter, thread_no);
+
+	char message[BUFF_SIZE], in_message[BUFF_SIZE], name[BUFF_SIZE];	
+
+	sprintf(message, "<<Server message: Type your name!>>\n\nYour name: ");
+
+	if(send (args -> sock[thread_no], message, sizeof(message)+1, 0)){
+
+		printf("Allow sent\n");
+	}
+
+	sendctrl(args -> sock[thread_no], message);
+
+	int recvs;
+
+	recvs = recv( args -> sock[thread_no], in_message, BUFF_SIZE, 0 );
+
+	if ( recvs == 0 ){
+		
+		terminate(thread_no, args -> sock[thread_no], args -> mysock);
+
+	}
+
+	else if ( recvs < 0)
+		printf("Recv error!\n");
+
+	printf("Client%i name: %s\n",thread_no+1, in_message);
+
+	strcpy(name, in_message);
+
+	for(int i = 0; i< sizeof name; i++ ){
+
+		if (name[i] == '\n')
+			name[i] = name[i+1];
+	}
+
+	sprintf(message, "<<Hello there %s!>>\n", name);
+
+	if(send (args -> sock[thread_no], message, sizeof(message)+1, 0)){
+
+		printf("Greeting sent\n");
+	}
+
+	sendctrl(args -> sock[thread_no], message);
+
+	CHOOSE: if (chat_rooms(args -> sock[thread_no], message, in_message, thread_no, arguments) == 0){
+
+		terminate(thread_no, args -> sock[thread_no], args -> mysock);
+	}
+
+	int psock = 0; 																	//partner's socket
+	for(;;){
+		if(rooms.choose[thread_no] == 1){
+			
+			if (rooms.r1sock[0] != 0 && rooms.r1sock[1] != 0){
+				printf("Partners connected.\n" );
+
+				for(int i = 0; i < 2 ; i++){
+
+					if(rooms.r1sock[i] != args -> sock[thread_no])
+						psock = rooms.r1sock[i];
+						
+				}
+
+				sprintf(message, "<<Connected to partner, you can start chatting.>>\n-------------------------------------------------");
+
+				send (args -> sock[thread_no], message, sizeof(message)+1, 0);
+
+				//if(rooms.change_count[thread_no] < 2)
+
+					sendctrl(args -> sock[thread_no], message);
+
+				break;
+			}
+		}
+
+			if(rooms.choose[thread_no] == 2){
+				if (rooms.r2sock[0] != 0 && rooms.r2sock[1] != 0){
+					printf("Partners connected.\n" );
+
+					for(int i = 0; i < 2 ; i++){
+
+						if(rooms.r2sock[i] != args -> sock[thread_no])
+							psock = rooms.r2sock[i];
+					}
+					sprintf(message, "<<Connected to partner, you can start chatting.>>\n-------------------------------------------------");
+
+					send (args -> sock[thread_no], message, sizeof(message)+1, 0);
+					
+					//if(rooms.change_count[thread_no] < 2)
+
+						sendctrl(args -> sock[thread_no], message);
+
+					break;
+
+				}
+			}
+	}
+
+	sprintf(message, "<<STOP_*_ACK>>");
+
+	send(args -> sock[thread_no], message, strlen(message)+1, 0);
+
+	sprintf(message, "Type '--help' for the control commands.");
+
+	send(args -> sock[thread_no], message, strlen(message)+1, 0);
+
+	while ( recvs != 0 ){
+
+		//BEJÖVŐ
+
+		recvs = recv( args -> sock[thread_no], in_message, BUFF_SIZE, 0 );
+
+		if ( recvs == 0 ){
+			printf("Client%i disconnected.\n", thread_no+1);
+
+			sprintf(message, "<<%s disconnected.>>\n\n << You can start a new conversation, or exit.>>\n<< If you don't know what to do type --help.>>", name);	
+			
+			if(send (psock, message, strlen(message)+1, 0)){
+
+				printf("Server message sent to %d\n", args -> sock[thread_no]);
+			}
+
+
+			for(int i = 0; i < 2 ; i++){
+
+				if(rooms.r1sock[i] == args -> sock[thread_no])
+					rooms.r1sock[i] = 0;	
+			}
+			terminate(thread_no, args -> sock[thread_no], args -> mysock);
+
+		}
+
+		else if ( recvs < 0)
+			printf("Recv error!\n");
+
+		else {
+			
+			printf("Client%i message: %s\n",thread_no+1, in_message);
+		}
+
+		if(!strcmp(in_message, "--exit\n")){
+			printf("Client%i disconnected.\n", thread_no+1);
+
+			sprintf(message, "<<%s disconnected.>>\n\n << You can start a new conversation, or exit.>>\n<< If you don't know what to do type --help.>>", name);	
+
+			if( rooms.choose[thread_no] == 1 ){
+
+				if(members(rooms.r1sock ) > 1){			
+
+					if(send (psock, message, strlen(message)+1, 0)){
+
+						printf("Server message sent to %d\n", args -> sock[thread_no]);
+					}
+				}
+			}
+
+			else if( rooms.choose[thread_no] == 2 ){
+
+				if(members(rooms.r2sock) > 1 ){			
+
+					if(send (psock, message, strlen(message)+1, 0)){
+
+						printf("Server message sent to %d\n", args -> sock[thread_no]);
+					}
+				}
+			}
+				
+			terminate(thread_no, args -> sock[thread_no], args -> mysock);
+
+			break;
+
+		}
+	
+		else if (!strcmp(in_message, "--help\n")){
+			sprintf(message, "<<'--exit' - leave the room and quit.>>\n<<'--change' - Changes room... surpriseee>>\n");
+			
+			send ( args -> sock[thread_no], message, strlen(message)+1, 0);
+
+			continue;
+		}
+
+		else if(!strcmp(in_message, "--change\n")){
+			
+			sprintf(message, "\r<<%s changed  room ... This room is closed, exit or change room.>>\n", name);
+
+			send(psock, message, strlen(message)+1, 0);
+
+			for (int i = 0; i < 2; i++){
+
+				if (rooms.r1sock[i] == args -> sock[thread_no] ){
+					rooms.r1sock[i] = 0;
+					psock = 0;
+					break;
+				}
+				else if (rooms.r2sock[i] == args -> sock[thread_no]){
+					rooms.r2sock[i] = 0;
+					psock = 0;
+					break;
+				}
+			}
+
+			goto CHOOSE;
+			
+				
+
+		}
+
+		else if ( !strcmp(in_message, "CLT_*_CHNG")){
+
+			printf("Clearing empty room...\n");
+			for (int i = 0; i < 2; i++){
+
+				if (rooms.r1sock[i] == args -> sock[thread_no] ){
+					rooms.r1sock[i] = 0;
+					psock = 0;
+					break;
+				}
+				else if (rooms.r2sock[i] == args -> sock[thread_no]){
+					rooms.r2sock[i] = 0;
+					psock = 0;
+					break;
+				}
+			}
+
+		}
+	
+
+		//KIMENő
+
+		if(strcmp(in_message, "CLT_*_ACK") && psock != 0){
+			sprintf(message, "%s: %s", name, in_message);
+
+			send (psock, message, strlen(message)+1, 0);
+
+			printf("Message sent to %d\n", psock);
+			
+		}
+			
+
+	}
+
+
+	terminate(thread_no, args -> sock[thread_no], args -> mysock);
+
+}
+
+
+int main( int argc, char* argv[]){
+
+	int sock, new_socket, c, *new_sock;
+
+	struct sockaddr_in server, client;
+
+	char* message;
+
+	sock = socket(AF_INET, SOCK_STREAM, 0);
+
+	if ( sock == -1 ){
+		
+		printf("Socket create error!\n");
+
+		return 1;
+	}
+
+	printf("Socket created.\n");
+
+	int on = 1;
+
+	setsockopt(sock, SOL_SOCKET, SO_REUSEADDR, &on, sizeof(on));
+
+	server.sin_family = AF_INET;
+
+	server.sin_addr.s_addr = INADDR_ANY;
+
+	server.sin_port = htons(PORT_NO);
+
+	args.counter = 0;
+
+	int retry = 0;
+
+	puts("Starting server\nPlease wait");
+
+	while(bind(sock, (struct sockaddr *) &server, sizeof(server)) == -1){
+
+		retry++;
+
+		if(retry%3 == 0 )
+			printf(".\n");
+
+		sleep(1);
+
+		if (retry > 30){
+			printf("Bind error!\n");
+			return -1;
+		}
+
+	}
+
+	printf("\nBind done.\n");
+
+	listen( sock, 4 );
+
+	printf("Waiting incoming connection...\n");
+
+	c = sizeof(struct sockaddr_in );
+	for (int i = 0; i < sizeof rooms.r1sock ; ++i)
+		{
+			rooms.r1sock[i] = 0;
+
+			rooms.r2sock[i] = 0;
+		}
+	for (int i = 0; i < MAX_CLIENT; ++i)
+	{
+		rooms.change_count[i] = 0;
+	}
+
+	while ( new_socket = accept( sock, ( struct sockaddr *)&client, (socklen_t* ) &c )){
+
+		printf("Connection accepted\n");
+	
+		message = "<<Server message: Connection accepted>>\n";
+
+		if ( send (new_socket, message, strlen(message)+1, 0) == -1 )
+
+			printf("Cannot send :/\n");
+
+		sendctrl(new_socket, message);
+
+		pthread_t thread;
+
+		new_sock = malloc(1);
+
+		*new_sock = new_socket;
+
+		int recvs = 0;
+
+		args.mysock = sock;
+
+		args.sock[args.counter] = new_socket;
+
+		args.counter++;
+
+		if(args.counter > MAX_CLIENT){
+
+			message = "<<Server message: We are full, sorry. Try again later...>>";
+
+			send(new_socket, message, strlen(message)+1, 0);
+
+			close(new_socket);
+
+			args.counter --;
+
+			if (send (new_socket, message, strlen(message)+1, 0) > 0)
+
+				printf("Deny send OK\n");
+
+		}
+
+		else if ( pthread_create ( &thread, NULL, handler, (void*) &args) < 0){
+
+			message = "<<Server message: Something went wrong, sorry. Try again later...>>";
+
+			close(new_socket);
+
+			args.counter --;
+
+			send (new_socket, message, strlen(message)+1, 0);
+
+			sendctrl(*new_sock, message);
+
+				
+
+		}
+	
+		else if ( new_socket < 0 ){
+
+			printf("Accept error!\n");
+
+			return 3;
+		}
+
+
+	}
+
+	close(sock);
+
+	return 0;
+}
